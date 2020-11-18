d3c87ab4a91fb48c33dcd4817c5ec1d3feed431e
pavanag123 <54305516+pavanag123@users.noreply.github.com>
2020-11-18 09:41:13 +0530

    Create lab1_math.sh

---
+++ lab1_math.sh
@@ -0,0 +1,8 @@
+echo "enter the values of two operands"
+read a
+read b
+echo "Addition : `expr $a + $b`"
+echo "Subtraction : `expr $a - $b`"
+echo "Multiplication : `expr $a \* $b`"
+echo "Division : `expr $a / $b`"
+echo "Remainder : `expr $a % $b`"
